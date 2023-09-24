6046d7acd122a44f5f2998caccfa99f966c63795
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-09-24 16:05:00 +0300

    displayed the cost of website layout in the console and fixed it

--- script.js
+++ script.js
@@ -9,16 +9,27 @@ console.log(
   "Стоимость экрана: " +
     screenPrice +
     "₽/" +
-    screenPrice * 96.3 +
+    Math.floor(screenPrice / 96.3) +
     "$/" +
-    screenPrice * 2.62 +
+    Math.floor(screenPrice / 2.62) +
     " гривен/" +
-    screenPrice * 13.2 +
+    Math.floor(screenPrice / 13.2) +
     " юаней"
 );
 const rollback = 14;
 console.log(rollback);
 const fullPrice = 100000;
 console.log(typeof fullPrice);
+console.log(
+  "Стоимость разработки сайта: " +
+    fullPrice +
+    "₽/" +
+    Math.floor(fullPrice / 96.3) +
+    "$/" +
+    Math.floor(fullPrice / 2.62) +
+    " гривен/" +
+    Math.floor(fullPrice / 13.2) +
+    " юаней"
+);
 const adaptive = true;
 console.log(typeof adaptive);
