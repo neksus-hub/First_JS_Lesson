485b33ba8d12a973f9359cb26faf5fdbc82558c5
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-09-24 15:31:25 +0300

    add variables

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+alert("Hello, World!");
+console.log("Hello, World!");
 
+const title = 1;
+const screens = 2;
+const screenPrice = 3;
+const rollback = 4;
+const fullPrice = 5;
+const adaptive = 6;
