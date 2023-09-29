bc4a00dd64a88987950bef5b2df1627c8c2fb106
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-09-30 01:21:49 +0300

    реализовал функцию gallServicePrices

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
 "use strict";
 // Блок переменных
-let title = "PROJECT";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 1613;
+
+let title = prompt("Как называется Ваш проект?");
+let screens = prompt("Какие экраны нужно разработать?");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
 const rollback = 14;
 let fullPrice = 100000;
-let adaptive = true;
-
-let service1;
-let servicePrice1;
-let service2;
-let servicePrice2;
 let servicePrecentPrice;
+let allServicePrices;
 
 //Блок объявления функций
 
+const getRollBackMessage = function (price) {
+  if (price >= 30000) {
+    return "Даем скидку в 10%";
+  } else if (price > 15000 && price < 30000) {
+    return "Даем скидку в 5%";
+  } else if (price < 15000 && price > 0) {
+    return "Скидка не предусмотрена";
+  } else if (price <= 0) {
+    return "Что-то пошло не так";
+  }
+};
+
+const gallServicePrices = function () {
+  allServicePrices = servicePrice1 + servicePrice2;
+};
+gallServicePrices();
+
 //Функциональный блок
-
-title = prompt("Как называется Ваш проект?");
-screens = prompt("Какие экраны нужно разработать?");
-screenPrice = +prompt("Сколько будет стоить данная работа?");
-adaptive = confirm("Нужен ли адаптив на сайте?");
-service1 = prompt("Какой дополнительный тип услуги нужен?");
-servicePrice1 = +prompt("Сколько это будет стоить?");
-service2 = prompt("Какой дополнительный тип услуги нужен?");
-servicePrice2 = +prompt("Сколько это будет стоить?");
-
 screens = screens.toLowerCase();
 
 fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
 servicePrecentPrice = fullPrice - Math.floor((fullPrice / 100) * rollback);
 
-if (fullPrice > 30000) {
-  console.log("Даем скидку в 10%");
-} else if (fullPrice > 15000 && fullPrice < 30000) {
-  console.log("Даем скидку в 5%");
-} else if (fullPrice < 15000 && fullPrice > 0) {
-  console.log("Скидка не предусмотрена");
-} else if (fullPrice <= 0) {
-  console.log("Что-то пошло не так");
-}
-
 // мусорный блок
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 
+console.log(allServicePrices);
+console.log(getRollBackMessage(fullPrice));
 console.log(screens);
 console.log(screens.length);
 console.log(screens.split(","));
