d6cafa7b15f18722bad15de2ebedab8af792192e
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-09-30 15:27:38 +0300

    почистил выводы в консоль

--- script.js
+++ script.js
@@ -61,46 +61,6 @@ console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 
-console.log(getServicePercentPrices());
-console.log(ucFirst(title));
-console.log(fullPrice);
-console.log(allServicePrices);
+console.log(screens);
 console.log(getRollBackMessage(fullPrice));
-console.log(screens);
-console.log(screens.length);
-console.log(screens.split(","));
-console.log(screenPrice);
-console.log(
-  "Стоимость экрана: " +
-    screenPrice +
-    "₽/" +
-    Math.floor(screenPrice / 96.3) +
-    "$/" +
-    Math.floor(screenPrice / 2.62) +
-    " гривен/" +
-    Math.floor(screenPrice / 13.2) +
-    " юаней"
-);
-console.log(rollback);
-console.log(
-  "Стоимость разработки сайта: " +
-    fullPrice +
-    "₽/" +
-    Math.floor(fullPrice / 96.3) +
-    "$/" +
-    Math.floor(fullPrice / 2.62) +
-    " гривен/" +
-    Math.floor(fullPrice / 13.2) +
-    " юаней"
-);
-console.log(Math.floor(fullPrice * (rollback / 100)));
-console.log(service1);
-console.log(servicePrice1);
-console.log(title);
-console.log(screens);
-console.log(screenPrice);
-console.log(adaptive);
-console.log(service2);
-console.log(servicePrice2);
-console.log(fullPrice);
-console.log(servicePrecentPrice);
+console.log(getServicePercentPrices());
